49d9ead1de0f336ea07bca7e5f0cf8a911bcac0e
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2024-11-02 03:14:57 +0900

    fix bug

--- server/src/main/java/com/hackathon/server/gpt/controller/GptController.java
+++ server/src/main/java/com/hackathon/server/gpt/controller/GptController.java
@@ -2,6 +2,7 @@ package com.hackathon.server.gpt.controller;
 
 import com.hackathon.server.gpt.dto.ImageUrl;
 import com.hackathon.server.gpt.dto.TextMessage;
+import com.hackathon.server.gpt.dto.TouristDto;
 import com.hackathon.server.gpt.dto.response.GptResponse;
 import com.hackathon.server.gpt.service.GptService;
 import com.hackathon.server.user.model.TouristAttraction;
@@ -22,7 +23,7 @@ public class GptController {
     private final TouristAttractionRepository touristAttractionRepository;
 
     @GetMapping("/image")
-    public List<TouristAttraction> imageAnalysis(@RequestParam String imageUrl) {
+    public TouristDto imageAnalysis(@RequestParam String imageUrl) {
         ImageUrl imageUrl1 = new ImageUrl(imageUrl);
         GptResponse gptResponse = gptService.requestImageAnalysis(imageUrl1);
         TextMessage message = (TextMessage) gptResponse.getChoices().get(0).getMessage();
@@ -30,16 +31,23 @@ public class GptController {
         System.out.println("text = " + text);
 
         List<TouristAttraction> touristAttractions = new ArrayList<>();
+        boolean flag = false;
 
         String[] places = text.split(", ");
         for (String place : places) {
+            if (place.equals("-1")) {
+                continue;
+            } else if (place.equals("1")) {
+                flag = true;
+                continue;
+            }
             touristAttractions.add(
                     touristAttractionRepository.findByName(place)
                             .orElseThrow());
         }
-
-        return touristAttractions;
+        return TouristDto.builder()
+                .flag(flag)
+                .touristAttractions(touristAttractions)
+                .build();
     }
-
-
 }
