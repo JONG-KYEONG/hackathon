68e7caa294980d6d374acbd7fdeaa157ceb59f01
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2024-11-02 04:53:52 +0900

    fix bug

--- server/src/main/java/com/hackathon/server/gpt/dto/TouristDto.java
+++ server/src/main/java/com/hackathon/server/gpt/dto/TouristDto.java
@@ -6,7 +6,6 @@ import lombok.Builder;
 
 @Builder
 public record TouristDto(
-        List<TouristAttraction> touristAttractions,
-        boolean flag
+        List<TouristAttraction> touristAttractions
 ) {
 }
