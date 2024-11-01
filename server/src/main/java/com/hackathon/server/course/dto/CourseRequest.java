63b56e99fe5b8c5983b03b047692162551a13c8c
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-01 23:54:11 +0900

    feat: api 수정

--- server/src/main/java/com/hackathon/server/course/dto/CourseRequest.java
+++ server/src/main/java/com/hackathon/server/course/dto/CourseRequest.java
@@ -3,10 +3,10 @@ package com.hackathon.server.course.dto;
 import java.util.List;
 
 public record CourseRequest (
-        Integer startX,
-        Integer startY,
-        Integer endX,
-        Integer endY,
+        Double startX,
+        Double startY,
+        Double endX,
+        Double endY,
         String contents
 ){
 }
