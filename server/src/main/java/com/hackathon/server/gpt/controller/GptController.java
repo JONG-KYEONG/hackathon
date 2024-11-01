8625deda94639c9b95820f76d75849d8a4ce0254
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2024-11-02 01:00:58 +0900

    add after gpt call logic

--- server/src/main/java/com/hackathon/server/gpt/controller/GptController.java
+++ server/src/main/java/com/hackathon/server/gpt/controller/GptController.java
@@ -1,8 +1,13 @@
 package com.hackathon.server.gpt.controller;
 
 import com.hackathon.server.gpt.dto.ImageUrl;
+import com.hackathon.server.gpt.dto.TextMessage;
 import com.hackathon.server.gpt.dto.response.GptResponse;
 import com.hackathon.server.gpt.service.GptService;
+import com.hackathon.server.user.model.TouristAttraction;
+import com.hackathon.server.user.repository.TouristAttractionRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class GptController {
     private final GptService gptService;
+    private final TouristAttractionRepository touristAttractionRepository;
 
     @GetMapping("/image")
-    public String imageAnalysis(@RequestParam String imageUrl) {
+    public List<TouristAttraction> imageAnalysis(@RequestParam String imageUrl) {
         // TODO: GPT 호출 Service 실행
         ImageUrl imageUrl1 = new ImageUrl(imageUrl);
         GptResponse gptResponse = gptService.requestImageAnalysis(imageUrl1);
-        System.out.println(gptResponse.getChoices());
-        // TODO: 주변 맛집 Service 실행 ??
-        return "";
+        TextMessage message = (TextMessage) gptResponse.getChoices().get(0).getMessage();
+        String text = message.getContent();
+        System.out.println("text = " + text);
+
+        List<TouristAttraction> touristAttractions = new ArrayList<>();
+
+        String[] places = text.split(",");
+        for (String place : places) {
+            touristAttractions.add(
+                    touristAttractionRepository.findByName(place)
+                            .orElseThrow());
+        }
+
+        return touristAttractions;
     }
 
 
